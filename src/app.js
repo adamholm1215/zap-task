7d80d22ad7300127b08e9d0c403fdd0182278e5f
devdarkhorse <maxmeland1215@gmail.com>
2023-02-01 18:12:50 +0100

    add zaptic route and pages

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+// import node modules
 import express from "express";
+
+// import routes
+import router from "./routes/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const port = 3333;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello from Zaptic! 👋💜");
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "/"));
+app.set("view engine", "ejs");
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Listening: http://localhost:${port}`);
