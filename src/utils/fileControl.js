cacb58d66f0e7d1063c6f43d429c2b05752de4a7
devdarkhorse <maxmeland1215@gmail.com>
2023-02-01 18:44:11 +0100

    add function that get users from csv

---
+++ src/utils/fileControl.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+
+export function getUserFromCSV(filename) {
+  
+  let csv = fs.readFileSync(filename);
+  let array = csv.toString().split("\r");
+  let users = [];
+  let headers = array[0].split(",");
+
+  for (let i = 1; i < array.length; i++) {
+    let obj = {};
+    let strArray = array[i].split(",");
+
+    for (let j = 0; j < headers.length; j++) {
+      if (j === 0) {
+        obj = {
+          ...obj,
+          id: strArray[0].replace(/(\r\n|\n|\r)/gm, ""),
+        };
+        continue;
+      }
+      obj[headers[j]] = strArray[j];
+    }
+
+    users.push(obj);
+  }
+  
+  return { users, headers };
+}
